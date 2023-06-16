ea2ae44abc6830552d2f153ff50e229bd387a7be
fallwith <fallwith@gmail.com>
2023-06-15 21:26:36 -0700

    test6

--- .github/actions/issue_closer/index.js
+++ .github/actions/issue_closer/index.js
@@ -2,13 +2,36 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const name = 'James';
-  console.log(`Hello ${name}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  const number = github.context.payload.number;
+  console.log(`The number is ${number}`);
+
+
+  // const name = 'James';
+  // console.log(`Hello ${name}!`);
+
+  // const time = (new Date()).toTimeString();
+  // core.setOutput("time", time);
+
+
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
+
+
+
+  // const token = core.getInput('token');
+  // const octokit = github.getOctokit(token)
+
+
+  // const { data: pullRequest } = await octokit.rest.pulls.get({
+  //   owner: "fallwith",
+  //   repo: "gha",
+  //   pull_number: 123,
+  // });
+
+
+
+
 } catch (error) {
   core.setFailed(error.message);
 }
